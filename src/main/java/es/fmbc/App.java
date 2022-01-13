7201f4828c0c6866fbb47499d64b08e2cf3ec0ef
pakkk <paco.benitez.chico@gmail.com>
2022-01-13 12:21:49 +0100

    mi primer proyecto

---
+++ src/main/java/es/fmbc/App.java
@@ -0,0 +1,13 @@
+package es.fmbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
